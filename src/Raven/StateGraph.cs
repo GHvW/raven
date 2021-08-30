83c2d6f311c742b550f62ea156f3f5f3de377f37
Garrett van Wageningen <ghvanw283@gmail.com>
2021-08-29 19:26:59 -0500

    refactor 1 to use a reducer func

---
+++ src/Raven/StateGraph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven {
+
+    public record StateGraph<TState, TTransition>(
+        ImmutableDictionary<TState, ImmutableHashSet<Transition<TState, TTransition>>> States)
+        where TState : notnull
+        where TTransition : notnull;
+
+    public static class StateGraph {
+        public static StateGraph<TState, TTransition> Create<TState, TTransition>(
+            ISet<TState> states,
+            ISet<Transition<TState, TTransition>> transitions)
+            where TState : notnull
+            where TTransition : notnull {
+
+            return new StateGraph<TState, TTransition>(states.ToImmutableDictionary(
+                    state => state,
+                    state =>
+                        transitions
+                            .Where(transition => states.Contains(transition.To) && transition.From.Equals(state))
+                            .ToImmutableHashSet()));
+        }
+    }
+}
+
+//        public StateGraph<TState, TTransition> Transition(TTransition item) {
+//            var it = 
+//                this.states[this.CurrentState]
+//                    .FirstOrDefault(edge => edge.When.Equals(item));
+
+//            return it != null
+//                ? new States<TState, TTransition>(this.states, it.To)
+//                : this;
+//        }
+//    }
+//}
--- test/Raven.Test/UnitTest1.cs
+++ test/Raven.Test/UnitTest1.cs
@@ -3,12 +3,19 @@ using Xunit;
 
 namespace Raven.Test
 {
+    public record Thing<A>(A It, string Bull);
+
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            Thing<string>? it = null;
 
+            var result = it?.It;
+
+
+            Assert.NotNull(result);
         }
     }
 }
